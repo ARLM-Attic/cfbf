18902a238a6d311e5619de5c9d8d3b37b9f92b4f
SND\ssaliba_cp <SND\ssaliba_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2013-01-05 23:50:09 +0000

    First add to SVN
    git-svn-id: https://cfbf.svn.codeplex.com/svn@25859 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

---
+++ cfbf/msg2eml/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.CFBF;
+
+namespace msg2eml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linda.msg");
+            var cfbf = new System.IO.CFBF.CompoundFileBinaryFileFormatReader(filePath);
+
+
+            //var i = cfbf.DirectoryEntries;
+
+            //var reader = new CompoundDocumentReader();
+            //IList<ResultSet> lista = reader.Read( new FileStream(filePath, FileMode.Open));
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
