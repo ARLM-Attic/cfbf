88cff194856a0e02d2c93d768eece29570140958
SND\ssaliba_cp <SND\ssaliba_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2013-01-08 13:10:50 +0000

    A byte array helper to convert from byte array to array of uint
    git-svn-id: https://cfbf.svn.codeplex.com/svn@26114 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

---
+++ System.IO.CFBF/ByteArrayExtension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.IO.CFBF
+{
+    public static class ByteArrayExtension
+    {
+
+        public static uint[] ToUInt32(this byte[] buffer)
+        {
+            if (buffer.Length % 4 != 0)
+                throw new Exception();
+
+            List<uint> retArray = new List<uint>();
+            MemoryStream byteStream = null;
+
+            try
+            {
+                byteStream = new MemoryStream(buffer, false);
+
+                for (int i = 0; i < byteStream.Length / 4; i++)
+                {
+                    var bytes = byteStream.ReadBytes(4);
+                    uint value = BitConverter.ToUInt32(bytes, 0);
+                    retArray.Add(value);
+                }
+
+            }
+            finally
+            {
+                if (byteStream != null)
+                {
+                    byteStream.Close();
+                    byteStream.Dispose();
+                }
+            }
+
+            return retArray.ToArray();
+        }
+
+        public static uint[] ToUInt32(this byte[] buffer, params uint[] ignoreValues)
+        {
+            if (buffer.Length % 4 != 0)
+                throw new Exception();
+
+            List<uint> retArray = new List<uint>();
+            MemoryStream byteStream = null;
+
+            try
+            {
+                byteStream = new MemoryStream(buffer, false);
+                for (int i = 0; i < byteStream.Length / 4; i++) {
+                    var bytes = byteStream.ReadBytes(4);
+                    uint value = BitConverter.ToUInt32(bytes, 0);
+
+                    if (!ignoreValues.Contains(value))
+                        retArray.Add(value);
+                }
+            }
+            finally
+            {
+                if (byteStream != null)
+                {
+                    byteStream.Close();
+                    byteStream.Dispose();
+                }
+            }
+            return retArray.ToArray();
+        }
+
+        public static byte[] ReadBytes(this byte[] buffer, int offset, int count)
+        {           
+            var byteStream = new MemoryStream(buffer, false);
+            byteStream.Position = offset;
+
+            var bytes = byteStream.ReadBytes(count);
+            byteStream.Close();
+            byteStream.Dispose();
+
+            return bytes;
+        }
+    }
+}
