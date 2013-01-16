ff642b868080046f2a91beaa2f120c2c5796f559
SND\ssaliba_cp <SND\ssaliba_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2013-01-16 22:42:00 +0000

    git-svn-id: https://cfbf.svn.codeplex.com/svn@26472 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

--- System.IO.CFBF/InvalidCFBFException.cs
+++ System.IO.CFBF/InvalidCFBFException.cs
@@ -10,6 +10,24 @@ namespace System.IO.CFBF
     /// </summary>
     public class InvalidCFBFException : Exception
     {
+        public InvalidCFBFException()
+            : base()
+        {           
+        }
 
+        public InvalidCFBFException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidCFBFException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InvalidCFBFException(Runtime.Serialization.SerializationInfo info, Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
