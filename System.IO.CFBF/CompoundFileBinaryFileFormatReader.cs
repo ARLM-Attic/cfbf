60e4d38d3c0c7a6b29bc3376261c41589b625c07
SND\ssaliba_cp <SND\ssaliba_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2013-01-17 01:52:22 +0000

    Correct Dispose method
    git-svn-id: https://cfbf.svn.codeplex.com/svn@26485 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

--- System.IO.CFBF/CompoundFileBinaryFileFormatReader.cs
+++ System.IO.CFBF/CompoundFileBinaryFileFormatReader.cs
@@ -333,10 +333,6 @@ namespace System.IO.CFBF
                 // if we add them, they need to be released here.
             }
             disposed = true;
-
-            // If it is available, make the call to the
-            // base class's Dispose(Boolean) method
-            Dispose(disposing);
         }
 
         private long SectorPosition(uint SecID)
